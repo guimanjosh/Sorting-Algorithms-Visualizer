6c579c80c6f52c9b847bde9568d603c2573ef05c
guimanjosh <122503035+guimanjosh@users.noreply.github.com>
2023-05-23 00:36:14 -0700

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -6,10 +6,12 @@ console.log(canvas.height);
 let ctx = canvas.getContext("2d");
 ctx.fillStyle = "#658CBB";
 let rangeInput = document.getElementById('ScrollBar').value;
+let time = 8000;
+
 let array = [];
 for(let i = 0; i < rangeInput * 4; i++)
 {
-    array.push(Math.ceil(Math.random()*-canvas.height));
+    array.push(Math.floor(Math.random()*-(canvas.height-1)) - 1);
 }
 
 
@@ -17,12 +19,13 @@ function generateArray()
 {
     rangeInput = document.getElementById('ScrollBar').value;
     ctx.clearRect(0,0, canvas.width, canvas.height);
+    ctx.fillStyle = "#658CBB";
     xCoord = 0;
     array = [];
 
     for(let i = 0; i < rangeInput * 4; i++)
     {
-        array.push(Math.ceil(Math.random()*-canvas.height));
+        array.push(Math.floor(Math.random()*-(canvas.height-1)) - 1);
     }
     
     interval = Math.floor(canvas.width/array.length);
@@ -32,9 +35,37 @@ function generateArray()
         ctx.fillRect(xCoord, canvas.height, interval, array[i]);
         xCoord = xCoord + interval;
     }
+
+    time = (8000/(array.length));
 }
 
 generateArray();
+
+//Helper Methods to update the board
+function changeColor(color, x, h)
+{
+    ctx.fillStyle = color;
+    ctx.fillRect(x, canvas.height, interval, h);
+}
+
+function delay (ms) {
+    return new Promise((resolve,reject) => setTimeout(resolve,ms));
+}
+
+async function sorted()
+{
+    for(let i = 0; i < array.length; i++){
+        changeColor("#AAFF00", interval*i, array[i]);
+        await delay(1000/array.length);
+    }
+
+    for(let i = 0; i < array.length; i++){
+        changeColor("green", interval*i, array[i]);
+    }    
+}
+
+sorted();
+
 
 //Sorting Algorithms
 
