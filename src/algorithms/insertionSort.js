e27ab0d185f73189a45e0f3cfc10e626e3054ee2
Joshua Guiman <122503035+guimanjosh@users.noreply.github.com>
2023-06-22 14:22:50 -0700

    Add files via upload

--- src/algorithms/insertionSort.js
+++ src/algorithms/insertionSort.js
@@ -7,7 +7,8 @@ import {sorted} from "../script.js"
 import {array} from "../script.js"
 import {enable} from "../script.js"
 
-let colors = ["red","orange","yellow","white"];
+//Light blue (#00f7ff) & red
+let colors = ['#00f7ff', 'red'];
 
 export default async function insertionSort()
 {
@@ -15,27 +16,31 @@ export default async function insertionSort()
     for(let i = 1; i < array.length; i++)
     {
         let curr = array[i];
-        changeColor(colors[i%4], i*interval, array[i]);
         let j = i - 1;
         while(j >= 0 && Math.abs(array[j]) > Math.abs(curr))
         {
+            //Before Swap
+            changeColor(colors[1], (j+1) * interval, array[j+1]);
+            changeColor(colors[1], j * interval, array[j]);
             await delay(1000/array.length);
+            
+            //Swapping
             let temp = array[j];
             ctx.clearRect(j*interval, canvas.height, interval, array[j]);
             ctx.clearRect((j+1)*interval, canvas.height, interval, array[j+1]);
             array[j] = array[j+1];
             array[j+1] = temp;
-            changeColor("blue", (j+1) * interval, array[j+1]);
-            changeColor(colors[j%4], j * interval, array[j]);
+            changeColor(colors[0], (j+1) * interval, array[j+1]);
+            changeColor(colors[0], j * interval, array[j]);
             await delay(1000/array.length);
+            changeColor('blue', (j+1) * interval, array[j+1]);
             j = j - 1;
         }
-        //await delay(1000/array.length);
-        //changeColor("#white", (j+1)*interval, array[j+1])
+        
         await delay(1000/array.length);
         changeColor("blue",(j+1)*interval, array[j+1])
     }
     sorted();
-    await delay(0);
+    await delay(500);
     enable();
 }
