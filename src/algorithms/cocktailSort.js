f5a6d03c029ab0ae69269ccd60983e02ba89049d
Joshua Guiman <122503035+guimanjosh@users.noreply.github.com>
2023-06-22 13:55:53 -0700

    Add files via upload

---
+++ src/algorithms/cocktailSort.js
@@ -0,0 +1,90 @@
+import {canvas} from "../script.js"
+import {ctx} from "../script.js"
+import {interval} from "../script.js"
+import {delay} from "../script.js"
+import {changeColor} from "../script.js"
+import {sorted} from "../script.js"
+import {array} from "../script.js"
+import {enable} from "../script.js"
+
+export default async function cocktailSort(){
+    var n = array.length;
+    var swapped = true;
+    let start = 0;
+    let end = n - 1;
+
+    while(swapped){
+        for(let i = start; i < end; i++){
+            if(Math.abs(array[i]) > Math.abs(array[i+1])){
+                changeColor('red', (i+1) * interval, array[i+1]);
+                changeColor('red', i * interval, array[i]);
+                await delay(1000/array.length);
+
+                ctx.clearRect(i*interval, canvas.height, interval, array[i]);
+                ctx.clearRect((i+1)*interval, canvas.height, interval, array[i+1]);
+
+                let temp = array[i];
+                array[i] = array[i+1];
+                array[i+1] = temp;
+
+                changeColor('#00f7ff', (i+1) * interval, array[i+1]);
+                changeColor('#00f7ff', i * interval, array[i]);
+                await delay(1000/array.length);
+
+                changeColor("#658CBB", i * interval, array[i]);
+                changeColor("#658CBB", (i+1) * interval, array[i+1]);
+
+                swapped = true;
+            }
+        }
+
+        changeColor('blue', end * interval, array[end]);
+
+        if (swapped == false)
+        {
+            break;
+        }
+
+        swapped = false;
+
+        end = end - 1;
+
+        for(let i = end - 1; i > start - 1; i--)
+        {
+            if(Math.abs(array[i]) > Math.abs(array[i+1]))
+            {
+                changeColor('purple', (i+1) * interval, array[i+1]);
+                changeColor('purple', i * interval, array[i]);
+                await delay(1000/array.length);
+
+                ctx.clearRect(i*interval, canvas.height, interval, array[i]);
+                ctx.clearRect((i+1)*interval, canvas.height, interval, array[i+1]);
+
+                let temp = array[i];
+                array[i] = array[i+1];
+                array[i+1] = temp;
+
+                changeColor('yellow', (i+1) * interval, array[i+1]);
+                changeColor('yellow', i * interval, array[i]);
+                await delay(1000/array.length);
+
+                changeColor("#658CBB", i * interval, array[i]);
+                changeColor("#658CBB", (i+1) * interval, array[i+1]);
+
+                swapped = true;
+            }   
+        }
+        changeColor('blue', start * interval, array[start]);
+        start = start + 1;
+
+    }
+    for(let i = 0; i < array.length; i++)
+    {
+        await delay(1000/(array.length*array.length));
+        changeColor('blue', i * interval, array[i]);
+    }
+    
+    sorted();
+    await delay(500);
+    enable();
+}
